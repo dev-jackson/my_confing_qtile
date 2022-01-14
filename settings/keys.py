07055efa45c5dfa3270a52fbd1def9a72a464016
dev-jackson <angeljackson0933@gmail.com>
2022-01-14 08:31:13 -0500

    add new keys

--- settings/keys.py
+++ settings/keys.py
@@ -25,7 +25,7 @@ keys = [Key(key[0], key[1], *key[2:]) for key in  [
     ([mod], "t", lazy.spawn('telegram-desktop')),
     ([mod], "f", lazy.spawn('firefox-developer-edition --force-device-scale-factor=1.5')),
     ([mod,"mod1"], "c", lazy.spawn('chromium --force-device-scale-factor=1.1')),
-    ([mod, "shift"], "b", lazy.spawn('brave --force-device-scale-factor=1.2')),
+    ([mod, "shift"], "b", lazy.spawn('brave --force-device-scale-factor=1.15')),
     ([mod,"mod1"], "m", lazy.spawn('min')),
     ([mod, "shift"], "w", lazy.spawn('whatsapp-nativefier-dark  --force-device-scale-factor=1.2')),
     # Switch between windows
